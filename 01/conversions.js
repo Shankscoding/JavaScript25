a31385410f9860f746176bbc3c2bb7dee6a1d1b4
Shanks <shashankmanohar2607@gmail.com>
2025-06-03 12:36:18 +0000

    third push

---
+++ 01/conversions.js
@@ -0,0 +1,32 @@
+let age = '33';
+
+let variable = "shanks"
+
+console.log(typeof age)
+console.log(typeof (age))
+
+
+let ValueinNumber = Number(age);
+let Valueofvariable =Number(variable);
+console.log(typeof ValueinNumber);
+console.log(ValueinNumber);
+console.log(typeof Valueofvariable);
+console.log(Valueofvariable);
+
+let nu = null; // typeconversion of null is 0
+console.log(Number(nu));
+let u = undefined; // typeconversion of undefined in Nan
+
+console.log(Number(u))
+// true is 1 and false is 0
+
+let isTrue = "h"
+let isFalse =""
+//console.log(\n);
+console.log();
+
+console.log(Boolean(isTrue));
+console.log(Boolean(isFalse))
+let tr=true;
+console.log();
+console.log(typeof tr);
---
+++ 01/operations.js
@@ -0,0 +1,8 @@
+console.log("1"+2+2);
+console.log(2+2+"1");
+
+let tru=true;
+console.log(+tru)
+
+tru++
+console.log(tru);
