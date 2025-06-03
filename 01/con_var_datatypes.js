e9f4368b198883af268244e3930ee980efa9d8f2
Shanks <shashankmanohar2607@gmail.com>
2025-06-03 10:28:28 +0000

    first file push

---
+++ 01/con_var_datatypes.js
@@ -0,0 +1,6 @@
+const accountID = 1
+let accountEmail = "shanks@gmail.com"   
+var accountPassword = "1234"
+accountCity = "Ranchi"
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
