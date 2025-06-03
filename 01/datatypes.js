7af6e4954821235596e476ebaf3842aedccdd803
Shanks <shashankmanohar2607@gmail.com>
2025-06-03 10:43:37 +0000

    second push

---
+++ 01/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+// use strict is used to work according to new JS norms
+
+//alert ("hello") used with html in console
+
+console.log("yoo");
+ 
+//bigint
+//symbol is used for uniqueness
+
+//object
+
+console.log(typeof null); // type of null is object
+
