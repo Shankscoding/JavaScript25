c7d90b8fdb5235c22d38988328b3d4bdf7dd6f77
Shanks <shashankmanohar2607@gmail.com>
2025-06-04 04:42:00 +0000

    third push

---
+++ 01/comparisions.js
@@ -0,0 +1,19 @@
+console.log("2" > 1);
+console.log("0">1)
+// converts it to string
+// typescript does not compare different data types
+console.log();
+console.log();
+console.log(null > 0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null ==0);
+console.log(null <=0);
+
+console.log();
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined <=0);
+console.log(undefined >0);
+console.log(undefined <0);
+//undefined is always false 
