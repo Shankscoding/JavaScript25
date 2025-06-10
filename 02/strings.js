5c0c497930bf116b8d3b6745be6f7432fed8b86f
Shanks <shashankmanohar2607@gmail.com>
2025-06-10 18:59:37 +0000

    first push

---
+++ 02/strings.js
@@ -0,0 +1,13 @@
+const name = "Shanks"
+const age = 20;
+console.log(`My Name is ${name} and my age is ${age}`)
+// string intropulation
+
+//string as an object 
+let Name = new String('SHASHANK MANOHAR ')
+console.log(Name);
+// in console screen of web browser, it will show
+// Name as an object with too many properties
+
+console.log(Name[3]);
+console.log(Name.__proto__)
